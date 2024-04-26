2f2a7a0367b4f0e03deda9d1dc8b2f768e6c43a6
Fontana <162226403+fontanaGu@users.noreply.github.com>
2024-04-26 18:05:46 -0300

    Re-up com fork

--- Adivinhacao.ConsoleApp/Program.cs
+++ Adivinhacao.ConsoleApp/Program.cs
@@ -1,125 +1,125 @@
-﻿namespace Adivinhacao.ConsoleApp
+﻿namespace Adivinhacao.ConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        decimal totalDePontos = 1000;
+
+        var numeroSecreto = ObterNumeroSecreto();
+
+        ApresentarMenu();
+
+        var dificuldade = EscolherDificuldade();
+
+        var totalDeTentativas = ObterTotalDeTentativas(dificuldade);
+
+        for (var tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
         {
-            decimal totalDePontos = 1000;
-
-            int numeroSecreto = ObterNumeroSecreto();
-
             ApresentarMenu();
 
-            int dificuldade = EscolherDificuldade();
+            Console.WriteLine($"\nTentativa {tentativa} de {totalDeTentativas}");
 
-            int totalDeTentativas = ObterTotalDeTentativas(dificuldade);
+            var numeroDigitado = ObterNumeroDigitado();
 
-            for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
-            {
-                ApresentarMenu();
+            var jogadorAcertou = AvaliarChute(numeroSecreto, numeroDigitado);
 
-                Console.WriteLine($"\nTentativa {tentativa} de {totalDeTentativas}");
+            if (jogadorAcertou) break;
 
-                int numeroDigitado = ObterNumeroDigitado();
+            totalDePontos = CalcularNovaPontuacao(totalDePontos, numeroSecreto, numeroDigitado);
 
-                bool jogadorAcertou = AvaliarChute(numeroSecreto, numeroDigitado);
-
-                if (jogadorAcertou) break;
-
-                totalDePontos = CalcularNovaPontuacao(totalDePontos, numeroSecreto, numeroDigitado);
-
-                if (tentativa >= totalDeTentativas)
-                    Console.WriteLine("Que azar, tente novamente!");
-
-                Console.ReadLine();
-            }
+            if (tentativa >= totalDeTentativas)
+                Console.WriteLine("Que azar, tente novamente!");
 
             Console.ReadLine();
         }
 
-        private static int ObterNumeroSecreto()
-        {
-            Random aleatorio = new Random();
-            int numeroSecreto = aleatorio.Next(1, 21);
+        Console.ReadLine();
+    }
 
-            return numeroSecreto;
+    private static int ObterNumeroSecreto()
+    {
+        var aleatorio = new Random();
+        var numeroSecreto = aleatorio.Next(1, 21);
+
+        return numeroSecreto;
+    }
+
+    private static decimal CalcularNovaPontuacao(decimal totalDePontos, int numeroSecreto, int numeroDigitado)
+    {
+        decimal pontosPerdidos = Math.Abs((numeroDigitado - numeroSecreto) / 2);
+        totalDePontos -= pontosPerdidos;
+
+        Console.WriteLine($"Você fez {totalDePontos} pontos!");
+
+        return totalDePontos;
+    }
+
+    private static bool AvaliarChute(int numeroSecreto, int numeroDigitado)
+    {
+        if (numeroDigitado == numeroSecreto)
+        {
+            Console.WriteLine("\nVocê acertou! O número secreto era: " + numeroSecreto);
+            return true;
         }
 
-        private static decimal CalcularNovaPontuacao(decimal totalDePontos, int numeroSecreto, int numeroDigitado)
+        if (numeroDigitado > numeroSecreto)
         {
-            decimal pontosPerdidos = Math.Abs((numeroDigitado - numeroSecreto) / 2);
-            totalDePontos -= pontosPerdidos;
-
-            Console.WriteLine($"Você fez {totalDePontos} pontos!");
-
-            return totalDePontos;
+            Console.WriteLine("\nO número digitado foi maior que o número secreto.");
+        }
+        else if (numeroDigitado < numeroSecreto)
+        {
+            Console.WriteLine("\nO número digitado foi menor que o número secreto.");
         }
 
-        private static bool AvaliarChute(int numeroSecreto, int numeroDigitado)
-        {
-            if (numeroDigitado == numeroSecreto)
-            {
-                Console.WriteLine("\nVocê acertou! O número secreto era: " + numeroSecreto);
-                return true;
-            }
-            else if (numeroDigitado > numeroSecreto)
-            {
-                Console.WriteLine("\nO número digitado foi maior que o número secreto.");
-            }
-            else if (numeroDigitado < numeroSecreto)
-            {
-                Console.WriteLine("\nO número digitado foi menor que o número secreto.");
-            }
+        return false;
+    }
 
-            return false;
+    private static int ObterNumeroDigitado()
+    {
+        Console.WriteLine("\nDigite um número entre 1 e 20:");
+        var numeroDigitado = Convert.ToInt32(Console.ReadLine());
+        return numeroDigitado;
+    }
+
+    private static int ObterTotalDeTentativas(int dificuldade)
+    {
+        var totalDeTentativas = 0;
+
+        switch (dificuldade)
+        {
+            case 1:
+                totalDeTentativas = 15;
+                break;
+
+            case 2:
+                totalDeTentativas = 10;
+                break;
+
+            case 3:
+                totalDeTentativas = 5;
+                break;
         }
 
-        private static int ObterNumeroDigitado()
-        {
-            Console.WriteLine("\nDigite um número entre 1 e 20:");
-            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
-            return numeroDigitado;
-        }
+        return totalDeTentativas;
+    }
 
-        private static int ObterTotalDeTentativas(int dificuldade)
-        {
-            int totalDeTentativas = 0;
+    private static int EscolherDificuldade()
+    {
+        Console.WriteLine("\nEscolha a dificuldade que irá jogar: ");
+        Console.WriteLine("(1) Fácil\t(2) Médio\t(3) Difícil");
 
-            switch (dificuldade)
-            {
-                case 1:
-                    totalDeTentativas = 15;
-                    break;
+        var dificuldade = Convert.ToInt32(Console.ReadLine());
 
-                case 2:
-                    totalDeTentativas = 10;
-                    break;
+        return dificuldade;
+    }
 
-                case 3:
-                    totalDeTentativas = 5;
-                    break;
-            }
+    private static void ApresentarMenu()
+    {
+        Console.Clear();
 
-            return totalDeTentativas;
-        }
-
-        private static int EscolherDificuldade()
-        {
-            Console.WriteLine("\nEscolha a dificuldade que irá jogar: ");
-            Console.WriteLine("(1) Fácil\t(2) Médio\t(3) Difícil");
-
-            int dificuldade = Convert.ToInt32(Console.ReadLine());
-
-            return dificuldade;
-        }
-
-        private static void ApresentarMenu()
-        {
-            Console.Clear();
-
-            Console.WriteLine("***********************");
-            Console.WriteLine("* Jogo de Adivinhação *");
-            Console.WriteLine("***********************");
-        }
+        Console.WriteLine("***********************");
+        Console.WriteLine("* Jogo de Adivinhação *");
+        Console.WriteLine("***********************");
     }
 }
